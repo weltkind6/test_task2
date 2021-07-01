38063b61dab28fd5ef1da73bc2db02ee8b5f3ec3
weltkind6 <yaroslavzef@icloud.com>
2021-07-01 08:05:30 +0300

    statistic #2

--- test-task2/src/Components/Statistic/Statistic.jsx
+++ test-task2/src/Components/Statistic/Statistic.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Statistic.scss'
 import tab from '../../img/tab.svg'
+import stat1 from '../../img/stat1.svg'
+import stat2 from '../../img/stat2.svg'
+import stat3 from '../../img/stat3.svg'
 
 const Statistic = () => {
     return (
@@ -8,9 +11,15 @@ const Statistic = () => {
             <div className="Statistic__wrapper">
                 <div className='tab-wrapper'>
                     <div className='tab-button'>
-                        <div>123</div>
-                        <div>Статитстика</div>
-                        <img src={tab} alt='tab'/>
+                        <div className='stat-items'>
+                            <img src={stat1} alt="stat1"/>
+                            <img src={stat2} alt="stat2"/>
+                            <img src={stat3} alt="stat3"/>
+                        </div>
+                        <span>Статитстика</span>
+                        <div>
+                            <img src={tab} alt='tab'/>
+                        </div>
                     </div>
                     <div className='tab'>
                         <div className='tab-dropDown'>
